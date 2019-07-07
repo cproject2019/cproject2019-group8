3191edefecc5eefc56e2c0eaf6fd93c57d5ae8f0
liuboyangaa <45805635+liuboyangaa@users.noreply.github.com>
2019-07-07 18:33:57 +0800

    Project

---
+++ archer/archer/Include/findingpath.h
@@ -0,0 +1,24 @@
+#pragma once
+#pragma once
+#ifndef FINDINGPATH_H
+#define FINDINGPATH_H
+
+#include<vector>
+#include "game_object.h"
+
+class FindingPath 
+{
+public: 
+	std::vector<GameObject> openlist;
+	std::vector<GameObject> closedlist;
+	std::vector<GameObject> path;
+	GLint FindingPathValue;
+	GLvoid Init(std::vector<GameObject>);
+	std::vector<GameObject> FindingWay(GameObject goal, GameObject nowStay);
+private:
+	std::vector<GameObject> stays;
+	GLboolean FindingNBH(GameObject now, GameObject goal);
+	GLvoid PrintfPath(GameObject goal);
+	GLboolean checkIfNear(GameObject one, GameObject two);
+};
+#endif
